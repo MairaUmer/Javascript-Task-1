a88cc060e951f14cb655511241e608e9da87abf5
Maira Umer <mairah.khan08@gmail.com>
2023-04-06 10:56:44 +0500

    First Commit

---
+++ scriptfile/app.js
@@ -0,0 +1,17 @@
+document.write("<center><h1>Javascript Task 1:</h1></center>")
+
+// var creation i am using namimg convention of camelCase
+var zakatPercentage = 0.025
+var zakatValue = 2
+
+// input using user prompt
+var userInput = prompt("Enter Your Monthly Zakat")
+
+// conversion of string prompt to number
+console.log(typeof(0.025))
+
+// multiple of zakat percentage and user input
+var result = 0.025*2
+console.log(result)
+
+alert('Your zakat value is ' + result )
